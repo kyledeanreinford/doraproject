7158f11bf0a97b98585d826c1eb7c87ee5bc1565
KyleBarton <kjbarton4@gmail.com>
2024-01-31 15:26:27 -0700

    add test outline

--- index.js
+++ index.js
@@ -6,13 +6,16 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(`
+
+  let responseHtml = `
   Build: green,
   Velocity: 1,
   Volatility: 2,
   Release Count: 100000000000
   `
-)});
+
+  res.end(responseHtml)
+});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
---
+++ test.sh
@@ -0,0 +1,7 @@
+node index.js & 
+
+sleep 4
+
+echo `curl http://localhost:3000`
+
+killall node
