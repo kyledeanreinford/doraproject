bbc0709c2096c5218f07506f01582ae866e7253d
Kyle Dean Reinford <kyledeanreinford@gmail.com>
2024-01-31 15:54:07 -0700

    Calls github with permissions

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import http from 'http';
-
+import { Octokit }  from "@octokit/rest";
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -7,14 +7,30 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
-  let responseHtml = `
-  Build: green,
-  Velocity: 1,
-  Volatility: 2,
-  Release Count: 100000000000
-  `
+  // Octokit.js
+// https://github.com/octokit/core.js#readme
+  const octokit = new Octokit({
+    auth: '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'
+  });
 
-  res.end(responseHtml)
+  octokit.request('GET /repos/{owner}/{repo}/actions/workflows', {
+    owner: 'kyledeanreinford',
+    repo: 'doraproject',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  }).then(({data}) => {
+
+    let testVal = "TODO"
+    res.end(`
+      Build: green,
+      Velocity: 1,
+      Volatility: 2,
+      Release Count: 
+      Raw Output: ${testVal}
+      foo: ${data}
+      `
+    )});
 });
 
 server.listen(port, hostname, () => {
