c663bfb219bb2d19f8326b6529fe2bc87e7fa507
KyleBarton <kjbarton4@gmail.com>
2024-01-31 15:18:12 -0700

    basic rendering of dora stuff

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!');
-});
+  res.end(`
+  Build: green,
+  Velocity: 1,
+  Volatility: 2,
+  Release Count: 100000000000
+  `
+)});
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
